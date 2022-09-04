987fd8a6655cce348728c534ab157e9faa8ed040
karim <karimomar71@gmail.com>
2022-09-04 03:07:55 +0200

    Updating the Create and Insert scripts

--- DBMS/create_table.sh
+++ DBMS/create_table.sh
@@ -1,31 +1,40 @@
 #! /bin/bash
 # Style the Connected Line
+
 function db_connected() {
     db_name=$1
     typeset -i filler_length
     filler_length=(54-${#db_name})
     echo -n "| $(tput setaf 3)<$1 "
     for ((counter = 0; counter < filler_length; counter++)); do echo -n "-"; done
-    echo " Connected>$(tput setaf 2) |"
+    echo " Connected>$(tput setaf 4) |" #change font color to Blue
 }
+
 function create_with_check() {
-    if ! [[ "$entry" =~ (^"create"|"CREATE"%) ]]; then echo "Invalid SQL Statement"; return; fi;  
-    
-     #remove SQL specific words 
+  
+    #Check if the input is correct and if not print Invalid 
+    if ! [[ "$entry" =~ (^"CREATE"||"create"%) ]]; 
+    then echo "Invalid SQL Statement : $entry "; return; fi;  
+
+    #Strip input from all SQL words
     sql_line=$(echo "$entry" | sed -e 's/CREATE//g' -e 's/TABLE//g' | sed -e 's/create//g' -e 's/table//g')
 
-    #get table and check its existance
+
+    #Check if the table name entered before the separator exists
     table_name=$(echo "$sql_line" | awk -F'(' '{gsub(/^[ \t]+|[ \t]+$/, "",$1);print $1}')
-    if [[ -f "$table_name" ]]; then
-        echo "Error : Tabel Name Exists"
+    if [[ -f "$table_name" ]]; 
+    then
+        echo "Error : Table Name already Exists"s
         return
     fi
 
     #create table files
-    touch "$table_name" 2>>../../error.log || echo "Failed"
-    touch ".$table_name" 2>>../../error.log || echo "Failed"
+    touch "$table_name" 2>>../../error.log || echo "Failed"     #for actual data
+    touch ".$table_name" 2>>../../error.log || echo "Failed"    #for storing table metadata
+
 
     #clean in format : column1 int pk , column2 txt
+    #and droping the "),(" separators
     sql_line=$(echo "$sql_line" | awk -F'(' '{print $2}' | awk -F')' '{ gsub(/^[ \t]+|[ \t]+$/, "",$1); print $1}')
 
     #table columns number
@@ -34,16 +43,20 @@ function create_with_check() {
     has_pk=0
     table_fields=""
     table_headers=""
+
     #check for invalid datatype or more than one primary key
     for ((i = 1; i <= $table_columns_number; i++)); do
         column_name=$(echo "$sql_line" | awk -F',' '{print $'$i' }' | awk -F' ' '{print $1}')
         dataType=$(echo "$sql_line" | awk -F',' '{print $'$i' }' | awk -F' ' '{print $2}')
         primary_key=$(echo "$sql_line" | awk -F',' '{print $'$i' }' | awk -F' ' '{print $3}')
-        if (((dataType != "txt") && (dataType != "int"))); then
-            echo "Error : Invalid Data Type in Column ['$i']"
-            return
+     
+        #check for invalid datatype
+        if (((dataType != "txt") && (dataType != "int"))); then    
+            echo "Error : Invalid Data Type in Column ['$i']"     
         fi
-        if [ "$primary_key" = "pk" ]; then
+
+        #check if there is more than one primary key
+        if [ "$primary_key" = "pk" ]; then  
             if ((has_pk == 0)); then
                 has_pk=1
             else
@@ -51,21 +64,33 @@ function create_with_check() {
                 return
             fi
         fi
+
+        #output the fieled separator "|" between columns
         table_headers=$(echo "$table_headers" | awk -F'|' '{OFS=FS}END{$'$i'="'$column_name'"; print}')
-        if [ "$primary_key" = "pk" ]; then
-            table_fields="$column_name|$dataType|$primary_key"
-        else table_fields="$column_name|$dataType"
+        
+        #check if there is primary_key or not 
+        if [ "$primary_key" = "pk" ]; 
+        then
+             table_fields="$column_name|$dataType|$primary_key"
+        else 
+             table_fields="$column_name|$dataType"
         fi
-        echo "$table_fields" >>".$table_name" 2>>../../error.log || echo "Failed"
+        
+        #output the metadate into " .table_name " file
+        echo "$table_fields" >>".$table_name" 2>>../../error.log || echo "Failed" 
+
     done
+
+    #output the table_header to " table_name " file
     echo "$table_headers" >>"$table_name"
+
     echo "Created Successfully"
 }
 
 clear
 db_name=$1
 while true; do
-    tput setaf 2 #change font color to Green
+    tput setaf 4 #change font color to Blue
     echo "+---------------------------------------------------------------------+"
     db_connected "$db_name"
     echo "+---------------------------------------------------------------------+"
@@ -74,7 +99,7 @@ while true; do
     echo "| 1 -  Back to DB Menu                                                |"
     echo "| 2 -  Back to Main Menu                                              |"
     echo "+---------------------------------------------------------------------+"
-    tput setaf 4 #change font color to blue
+    tput setaf 4 #change font color to Yellow
     read -p "$(tput setaf 3)Enter SQL CREATE Statement : " entry
     case $entry in
     1) exit ;;
--- DBMS/insert_into.sh
+++ DBMS/insert_into.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 # Style the Connected Line
+
 function db_connected() {
 	db_name=$1
 	typeset -i filler_length
 	filler_length=(54-${#db_name})
 	echo -n "| $(tput setaf 3)<$1 "
 	for ((counter = 0; counter < filler_length; counter++)); do echo -n "-"; done
-	echo " Connected>$(tput setaf 2) |"
+	echo " Connected>$(tput setaf 4) |" #change font color to Blue
 }
 function insert_with_check() {
-	#remove SQL specific words
+	#Strip input from all SQL words.
 	sql_line=$(echo "$entry" | sed -e 's/INSERT//g' -e 's/INTO//g' -e 's/VALUES//g' | sed -e 's/insert//g' -e 's/into//g' -e 's/values//g')
 
 	#get table and check its existance
 	table_name=$(echo "$sql_line" | awk -F';' '{gsub(/^[ \t]+|[ \t]+$/, "",$1);print $1}')
-	if [[ ! -f "$table_name" ]]; then
+	if [[ ! -f "$table_name" ]]; 
+		then
 		echo "Error : Invalid Table Name"
 		return
 	fi
@@ -25,31 +27,49 @@ function insert_with_check() {
 	#get insert columns number
 	insert_columns_number=$(echo "$sql_line" | awk -F',' 'END{print NF}')
 
-	#check if the number of values matches the number of table columns
-	if ((insert_columns_number != table_columns_number)); then
+	#Check if the number of values matches the number of table columns
+	if ((insert_columns_number != table_columns_number)); 
+		then
 		echo "Error : Invalid Columns Number, Must Be $table_columns_number Values"
 		return
 	fi
 
-	#form the Record
+	#Form the Record
+
 	record=""
+
 	for ((i = 1; i <= $insert_columns_number; i++)); do
-		#strip fieled from all special charctars
-		value=$(echo "$sql_line"| awk -F';' '{print $2}'| sed -e 's/(//g' -e 's/)//g' | awk -F',' '{gsub(/^[ \t]+|[ \t]+$/, "",$'$i');print $'$i'}')
+
+		#Strip the input from all special characters and 
+		#Check if the entered value matchs the column metadata
+		value=$(echo "$sql_line"| awk -F';' '{print $2}'| sed -e 's/(//g' -e 's/)//g' |
+		awk -F',' '{gsub(/^[ \t]+|[ \t]+$/, "",$'$i');print $'$i'}')
+		
+		#Check if the entered value matchs the datatype
 		col_data_type=$(awk -F'|' '{if (NR=="'$i'") { print $2}}' ".$table_name")
+		
+		#Check if there is a PK
 		is_pk=$(awk -F'|' '{if (NR=="'$i'") { print $3}}' ".$table_name")
+
+		#Check if the entered value matchs the datatype and if not print invalid
 		case $value in
 			[a-zA-Z]*) if ((col_data_type != "txt")); then echo "Error : Invalid Value Data Type"; return; fi ;;
 			[0-9]*) if ((col_data_type != "int")); then echo "Error : Invalid Value Data Type" return; fi ;;
 			*) echo "Error : Invalid Value";return	;;
 		esac
+
+		#Check if there is a PK an if the value exists then print invalid
 		if ((is_pk == "pk")); then
 			value_exist=$(awk -v new_value="$value" -F'|' 'BEGIN{found=0} {if(NR!=1){if($"'$i'"==new_value)found=1}} END{print found}' "$table_name")
 			if ((value_exist == 1)); then echo "Error : Primary Key Exist"; return; fi
 		fi
+		
+		#Insert the value under it's column
 		record=$(echo "$record" | awk -v value="$value" -F'|' '{OFS=FS}{$"'$i'"=value; print}')
+
 	done
-	#append the Record1
+
+	#Append the Record1
 	echo "$record" >>"$table_name"
 	echo "Inserted Successfully"
 }
@@ -57,7 +77,7 @@ function insert_with_check() {
 clear
 db_name=$1
 while true; do
-	tput setaf 2 #change font color to Green
+	tput setaf 4 #change font color to Blue
 	echo "+---------------------------------------------------------------------+"
 	db_connected "$db_name"
 	echo "+---------------------------------------------------------------------+"
@@ -66,7 +86,7 @@ while true; do
 	echo "| 1 -  Back to DB Menu                                                |"
 	echo "| 2 -  Back to Main Menu                                              |"
 	echo "+---------------------------------------------------------------------+"
-	tput setaf 4 #change font color to blue
+	tput setaf 4 #change font color to Yellow
 	read -p "$(tput setaf 3)Enter SQL Delete Statement : " entry
 	case $entry in
 	1) exit ;;
